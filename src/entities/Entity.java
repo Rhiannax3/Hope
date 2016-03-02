27bba709984ecdb1ee9878b8066a5f190d8cf4c0
Rhiannax3 <Rhianna.Cloud@hotmail.com>
2016-03-02 19:26:41 +0000

    Nothing Important

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,14 +1,14 @@
 package entities;
 
 public class Entity {
+	
+	public Entity ()
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
-	createNamedQuery ("SELECT e FROM " + entity.getClass().getName().toLowerCase().substring(entity.getClass().getName().lastIndexOf(".") + 1) + " e");
+//	createNamedQuery ("SELECT e FROM " + entity.getClass().getName().toLowerCase().substring(entity.getClass().getName().lastIndexOf(".") + 1) + " e");
 	
 }
-
-JTHOMPSONNB
